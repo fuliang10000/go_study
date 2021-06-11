dbcbeb95dda45ac817e9fafa6028d809e67cc56c
fuliang <632249982@qq.com>
2021-06-11 14:52:21 +0800

    提交

--- class.go
+++ class.go
@@ -10,12 +10,26 @@ type Student struct {
 }
 
 func main()  {
-	Student := NewStudent(1, 'dfdf', true, 23.22)
-
+	Student := NewStudent(1, "Fu Liang", 23.22)
+	Student.SetName("JAY")
 	fmt.Println(Student)
 }
 
-func NewStudent(id uint, name string, male bool, score float64) *Student {
-	return &Student(id, name, male, score)
+//func NewStudent(id uint, name string, male bool, score float64) *Student {
+//	return &Student{id, name, male, score}
+//}
+func NewStudent(id uint, name string, score float64) *Student {
+	return &Student{id: id, name:name, score:score}
 }
 
+func (s Student) GetName() string  {
+	return s.name
+}
+
+func (s *Student) SetName(name string)  {
+	s.name = name
+}
+
+func (s Student) String() string {
+	return fmt.Sprintf("{id: %d, name: %s, male: %t, score: %f}", s.id, s.name, s.male, s.score)
+}
---
+++ testInterface.go
@@ -0,0 +1,6 @@
+package main
+
+type Math interface {
+	Add(i Integer) Integer
+	Multiply(i Integer) Integer
+}
